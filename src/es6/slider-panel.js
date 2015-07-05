59b88056383467e09f0018fa4e0eb235a7e8cc79
Takafumi Sekine <sekine@rabi.co.jp>
2015-07-06 02:42:53 +0900

    スライダー機能作成

--- src/es6/slider-panel.js
+++ src/es6/slider-panel.js
@@ -1,7 +1,30 @@
 var $ = require('jquery');
 
 export default class SliderPanel {
-  constructor($el) {
+  constructor(slider, $el) {
+    this.slider = slider;
     this.$el = $el;
+    this.width = 0;
+    this.height = 0;
+
+    this.setSize();
+    this.setBindings();
+ }
+
+  setSize() {
+    this.width = this.$el.width();
+    this.height = this.$el.height();
+  }
+
+  setPostion(position) {
+    this.$el.css(position);
+  }
+
+  setBindings() {
+    var self = this;
+    self.$el.on('click', function() {
+      var index = $(this).data('index');
+      self.slider.setPanel(index);
+    });
   }
 }
--- src/es6/slider.js
+++ src/es6/slider.js
@@ -4,5 +4,52 @@ var SliderPanel = require('./slider-panel.js');
 export default class Slider {
   constructor($el) {
     this.$el = $el;
+    this.width = 0;
+    this.height = 0;
+    this.panels = [];
+    this.panelIndex = 0;
+
+    this.setSize();
+    this.setPanels();
+    this.lineUp();
+ }
+
+  setSize() {
+    this.width = $(window).width();
+    this.height = $(window).height();
+  }
+
+  setPanels() {
+    var self = this;
+    self.panels = [];
+    self.$el.find('.slider-panel').each(function() {
+      self.panels.push(new SliderPanel(self, $(this)));
+    });
+  }
+
+  setPanel(index) {
+    this.panelIndex = index;
+    this.lineUp();
+  }
+
+  lineUp() {
+    this.panels.forEach((panel, i) => {
+      let position = {
+        top: this.height / 2 - panel.height / 2,
+        left: 0
+      };
+      if (i < this.panelIndex - 1) {
+        position.left = -this.width;
+      } else if (i === this.panelIndex - 1) {
+        position.left = -panel.width + this.width * 0.1;
+      } else if (i === this.panelIndex) {
+        position.left = this.width / 2 - panel.width / 2;
+      } else if (i === this.panelIndex + 1) {
+        position.left = this.width - this.width * 0.1;
+      } else if (i > this.panelIndex + 1) {
+        position.left = this.width;
+      }
+      panel.setPostion(position);
+    });
   }
 }
